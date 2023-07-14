d92c2b4d519f674a8cdf1f83e22bfddbc0d0b6b0
taehankim-dev <goldmini95@gmail.com>
2023-07-14 17:10:52 +0900

    api post data 변경

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,15 @@
+import {join} from 'path';
 import { readdirSync, readFileSync } from "fs";
 import matter from "gray-matter";
 import markdownToHtml from '@/lib/markdownToHtml';
 
+const postsDirectory = join(process.cwd(), '_posts');
+
 export const getAllPostData = () => {
-  const posts = readdirSync("_posts").map((file) => {
-    const post = readFileSync(`_posts/${file}`, 'utf-8');
+  const posts = readdirSync(postsDirectory).map((file) => {
+    const post = readFileSync(`${postsDirectory}/${file}`, 'utf-8');
     return matter(post).data;
   });
-
-  console.log("api posts : ", posts)
 
   return posts;
 }
